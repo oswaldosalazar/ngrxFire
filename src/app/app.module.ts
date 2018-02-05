feaa1dd8a693765b65abde8840e10e1c8756fe71
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2018-02-04 22:23:11 -0700

    Added devtools plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { simpleReducer } from './simple.reducer'
 import { postReducer } from './reducers/post.reducer'
 
 
@@ -19,9 +18,9 @@ import { postReducer } from './reducers/post.reducer'
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    StoreModule.forRoot({
-      post: postReducer,
-      message: simpleReducer
+    StoreModule.forRoot({ post: postReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 10
     })
   ],
   providers: [],
